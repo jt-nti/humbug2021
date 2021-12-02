8d73456c13a8b90faa148c2dab9b715751739a99
James Taylor <jamest@uk.ibm.com>
2021-12-02 10:20:14 +0000

    Day 2, part 2
    Signed-off-by: James Taylor <jamest@uk.ibm.com>

--- src/day2.rs
+++ src/day2.rs
@@ -36,6 +36,35 @@ pub fn part1(input_path: &PathBuf) {
     println!("Position * Depth = {}", position * depth);
 }
 
-pub fn part2(_input_path: &PathBuf) {
-    println!("TBC");
+pub fn part2(input_path: &PathBuf) {
+    let mut aim: u32 = 0;
+    let mut position: u32 = 0;
+    let mut depth: u32 = 0;
+
+    let content = std::fs::read_to_string(input_path).expect("could not read file");
+    for line in content.lines() {
+        let mut split = line.split(' ');
+        let direction = split.next().expect("Expected a direction");
+        let distance: u32 = split.next().expect("Expected a distance").parse().expect("Not a number!");
+
+        let command = match direction {
+            "up" => Command::Up(distance),
+            "down" => Command::Down(distance),
+            "forward" => Command::Forward(distance),
+            _ => panic!("Invalid command: {}!", direction)
+        };
+
+        match command {
+            Command::Up(d) => aim -= d,
+            Command::Down(d) => aim += d,
+            Command::Forward(d) => {
+                position += d;
+                depth += aim * d;
+            },
+        }
+    }
+
+    println!("Position = {}", position);
+    println!("Depth = {}", depth);
+    println!("Position * Depth = {}", position * depth);
 }
